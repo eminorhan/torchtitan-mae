1287a751528edf132728059244d135e9f0fe015a
Emin Orhan <aeminorhan@gmail.com>
2026-02-16 14:46:13 +0000

    add no mxp, no fa3 train config

--- train_segmentation_3d.sh
+++ train_segmentation_3d.sh
@@ -6,10 +6,10 @@
 #SBATCH --cpus-per-task=288
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=4
-#SBATCH --time=6:00:00
+#SBATCH --time=1:00:00
 #SBATCH --job-name=train_segmentation_3d
 #SBATCH --output=train_segmentation_3d_%A_%a.out
-#SBATCH --array=0-9%1
+#SBATCH --array=0
 
 # activate venv
 source /lustre/blizzard/stf218/scratch/emin/blizzardvenv/bin/activate
@@ -37,7 +37,7 @@ export GPUS_PER_NODE=4
 export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
 export MASTER_PORT=3442
 
-CONFIG_FILE=${CONFIG_FILE:-"./train_configs/dinov3_3d_vit7b_segmentor.toml"}
+CONFIG_FILE=${CONFIG_FILE:-"./train_configs/dinov3_3d_vit7b_segmentor_no_fa3.toml"}
 
 srun torchrun --nnodes $SLURM_NNODES --nproc_per_node $GPUS_PER_NODE --max_restarts 1 --node_rank $SLURM_NODEID --rdzv_id 101 --rdzv_backend c10d --rdzv_endpoint "$MASTER_ADDR:$MASTER_PORT" ./train_segmentation.py --job.config_file ${CONFIG_FILE}
 
