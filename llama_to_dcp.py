9105bb088e8d87852d9f183f96f8527ec9bf6724
Emin Orhan <aeminorhan@gmail.com>
2026-03-04 15:31:48 +0000

    update llama_to_dcp

--- llama_to_dcp.py
+++ llama_to_dcp.py
@@ -1,44 +1,68 @@
-import os
+import argparse
 import torch
 import torch.distributed.checkpoint as DCP
 from torch.distributed.checkpoint.format_utils import torch_save_to_dcp
 from dinov3.eval.segmentation.models import build_segmentation_decoder
 from pathlib import Path
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Load DINOv3 models and save them as DCP checkpoints.")
+    parser.add_argument("--torch_hub_path", type=Path, default=Path("/lustre/blizzard/stf218/scratch/emin/torch_hub"), help="Root of torch_hub")
+    parser.add_argument("--dinov3_repo_path", type=Path, default=Path("/lustre/blizzard/stf218/scratch/emin/dinov3"), help="DINOv3 repo path")
+    parser.add_argument("--dcp_root", type=Path, default=Path("/lustre/blizzard/stf218/scratch/emin/torchtitan-segmentation/outputs"), help="Root path where DCP checkpoints will be saved")
+    parser.add_argument("--decoder_type", type=str, choices=["linear", "m2f"], default="linear", help="Segmentation head type (choices: linear, m2f)")
+    parser.add_argument("--num_classes", type=int, default=64, help="Number of classes in the current semantic segmentation task")
+    return parser.parse_args()
 
 if __name__ == "__main__":
 
-    TORCH_HUB_PATH = "/lustre/blizzard/stf218/scratch/emin/torch_hub"  # this is where the dinov3 pth checkpoints are stored
-    DINOV3_REPO_PATH = "/lustre/blizzard/stf218/scratch/emin/dinov3"  # dinov3 repo path
-    DECODER_TYPE = "linear"  # segmentation head type
-    NUM_CLASSES = 64  # number of classes in semantic segmentation task
+    args = parse_args()
 
+    # Full path where the pretrained pth checkpoints are stored
+    BACKBONE_PTH_ROOT = args.torch_hub_path / "checkpoints"
+
+    # You can add a few more dinov3 checkpoints below
     BACKBONE_CKPT_DICT = {
-        # "dinov3_vit7b16_3D": "dinov3_vit7b16_pretrain_lvd1689m-a955f4ea.pth",
-        # "dinov3_vit7b16": "dinov3_vit7b16_pretrain_lvd1689m-a955f4ea.pth",
-        # "dinov3_vith16plus_3D": "dinov3_vith16plus_pretrain_lvd1689m-7c1da9a5.pth",
-        # "dinov3_vith16plus": "dinov3_vith16plus_pretrain_lvd1689m-7c1da9a5.pth",
-        # "dinov3_vitl16_3D": "dinov3_vitl16_pretrain_lvd1689m-8aa4cbdd.pth",
-        # "dinov3_vitl16": "dinov3_vitl16_pretrain_lvd1689m-8aa4cbdd.pth",
-        "dinov3_vitl16": "teacher_backbone_only_checkpoint_scratch-8aa4cbdd.pth",
-        # "dinov3_vitb16_3D": "dinov3_vitb16_pretrain_lvd1689m-73cec8be.pth",
-        # "dinov3_vitb16": "dinov3_vitb16_pretrain_lvd1689m-73cec8be.pth",
+        "dinov3_vit7b16_3D": "dinov3_vit7b16_pretrain_lvd1689m-a955f4ea.pth",
+        "dinov3_vit7b16": "dinov3_vit7b16_pretrain_lvd1689m-a955f4ea.pth",
+        "dinov3_vith16plus_3D": "dinov3_vith16plus_pretrain_lvd1689m-7c1da9a5.pth",
+        "dinov3_vith16plus": "dinov3_vith16plus_pretrain_lvd1689m-7c1da9a5.pth",
+        "dinov3_vitl16_3D": "dinov3_vitl16_pretrain_lvd1689m-8aa4cbdd.pth",
+        "dinov3_vitl16": "dinov3_vitl16_pretrain_lvd1689m-8aa4cbdd.pth",
+        "dinov3_vitb16_3D": "dinov3_vitb16_pretrain_lvd1689m-73cec8be.pth",
+        "dinov3_vitb16": "dinov3_vitb16_pretrain_lvd1689m-73cec8be.pth",
     }
 
-    BACKBONE_PTH_ROOT = "/lustre/blizzard/stf218/scratch/emin/torch_hub/checkpoints"
-    DCP_ROOT = "/lustre/blizzard/stf218/scratch/emin/torchtitan-mae/outputs"
-
-    torch.hub.set_dir(TORCH_HUB_PATH)
+    # Set torch_hub dir
+    torch.hub.set_dir(str(args.torch_hub_path))
 
     for backbone, backbone_pth in BACKBONE_CKPT_DICT.items():
-        bbone = torch.hub.load(DINOV3_REPO_PATH, backbone, source="local", weights=f"{BACKBONE_PTH_ROOT}/{backbone_pth}", pretrained=True, use_fa3=True)
-        model = build_segmentation_decoder(bbone, decoder_type=DECODER_TYPE, num_classes=NUM_CLASSES)
+        weights_path = BACKBONE_PTH_ROOT / backbone_pth
+        
+        bbone = torch.hub.load(
+            str(args.dinov3_repo_path), 
+            backbone, 
+            source="local", 
+            weights=str(weights_path), 
+            pretrained=True, 
+            use_fa3=True
+        )
+        
+        model = build_segmentation_decoder(
+            bbone, 
+            decoder_type=args.decoder_type, 
+            num_classes=args.num_classes
+        )
         model_state_dict = model.state_dict()
         print(f"Loaded and built model {backbone}...")
         print(f"Model: {model}")
 
-        dcp_path = Path(f"{DCP_ROOT}/{backbone}_{DECODER_TYPE}_cellmap_scratch/checkpoint/step-0")
-        storage_writer = DCP.filesystem.FileSystemWriter(dcp_path, thread_count=1)
+        # Construct full path where dcp checkpoint will be saved
+        dcp_path = args.dcp_root / f"{backbone}_{args.decoder_type}" / "checkpoint" / "step-0"
+        
+        # Make directories before writing
         dcp_path.mkdir(parents=True, exist_ok=True)
+        
+        storage_writer = DCP.filesystem.FileSystemWriter(dcp_path, thread_count=1)
         DCP.save({"model": model_state_dict}, storage_writer=storage_writer)
         print(f"Wrote DCP ckpt to {dcp_path}...")
